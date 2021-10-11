dd18755f591513f02c1a15e20413ac2c392caada
Samim_Zafar <samim.zafar@wearenova.co.uk>
2021-10-11 14:14:01 +0500

    Updates

---
+++ precache-manifest.7ae090aac2998387592767e7de58311e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "654e6042a10a4b1da500",
+    "url": "/redux-saga/static/js/main.f53f7cf6.chunk.js"
+  },
+  {
+    "revision": "7c92902313b668a48bb9",
+    "url": "/redux-saga/static/js/runtime~main.28fd39f8.js"
+  },
+  {
+    "revision": "1e421fa84acbe11d7e09",
+    "url": "/redux-saga/static/js/2.c0769b91.chunk.js"
+  },
+  {
+    "revision": "1f18232d63aadb34c0adc8bb23bed34f",
+    "url": "/redux-saga/index.html"
+  }
+];
